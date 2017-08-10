d55757a065987cd159a68279c56f71e161403072
Marlon O'Neil <hello@marlononeil.com>
2017-08-10 17:10:36 -0400

    Completed exercise 0, step 1

---
+++ exercise.rb
@@ -0,0 +1,14 @@
+#Create an array for each item below that contains the given information:
+#your favourite colours
+my_fav_colours = ["red", "blue", "green", "yellow"]
+
+#the age of you and your siblings/cousins/friends
+
+
+#flip a coin 5 times and record whether or not the result was 'heads'
+
+
+#your three favourite performing artists
+
+
+#your favourite colours again (this time as symbols instead of strings)
